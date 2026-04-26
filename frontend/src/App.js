48f6cb771afec25f57f2a5d99d1c3f782ae7b771
udbhavtanikella <udbhavtanikella@gmail.com>
2026-04-25 19:13:01 -0700

    no donate sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ import SafetyGuidelines from './pages/SafetyGuidelines';
 import ListingPolicy from './pages/ListingPolicy';
 import DMCAPolicy from './pages/DMCAPolicy';
 import messageService from './services/messageService';
-import DonateSidebar from './components/DonateSidebar';
+// import DonateSidebar from './components/DonateSidebar';
 import Footer from './components/Footer';
 
 function isTokenValid() {
@@ -114,8 +114,8 @@ function App() {
   return (
     <Router>
       <Navbar isAuthenticated={isAuthenticated} user={user} isAdmin={isAdmin} onLogout={handleLogout} unreadCount={unreadCount} />
-      <DonateSidebar />
-      <div className="page-content">
+      {/* <DonateSidebar /> */}
+<div className="page-content">
       <Routes>
         <Route 
           path="/login" 
